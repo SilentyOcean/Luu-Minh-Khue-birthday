54027ab8120e2fdbf9f4f8de6294ce18811a9312
SilentyOcean <hoq6884@gmail.com>
2025-09-10 09:48:07 -0500

    Added cake, fonts

--- cake.js
+++ cake.js
@@ -1,25 +1,202 @@
-// // first we need to create a stage
-// var stage = new Konva.Stage({
-//   container: 'container', // id of container <div>
-//   width: 500,
-//   height: 100,
-// });
+ const stage = new Konva.Stage({
+  container: "container",
+  width: 700,
+  height: 700});
 
-// // then create layer
-// var layer = new Konva.Layer();
+  const layer = new Konva.Layer();
+  stage.add(layer);
 
-// // create our shape
-// var circle = new Konva.Circle({
-//   x: stage.width() / 2,
-//   y: stage.height() / 2,
-//   radius: 70,
-//   fill: 'red',
-//   stroke: 'black',
-//   strokeWidth: 4,
-// });
 
-// // add the shape to the layer
-// layer.add(circle);
 
-// // add the layer to the stage
-// stage.add(layer);
+
+class Cake {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.radX = 100 * this.size;
+    this.radY = 50 * this.size;
+    this.candlesBlown = 0;
+  }
+
+  
+  blownCheck(){
+    if(this.candlesBlown == 18){
+      console.log("Confetti!");
+      coolConfetti();
+    }
+    
+  }
+
+
+  candles(count = 1) {
+  
+    
+    let angle = (2 * Math.PI / 18) * count;
+
+    // Ellipse parametric equation
+    let randX = this.x + (this.radX * 0.8) * Math.cos(angle);
+    let randY = this.y + (this.radY * 0.8) * Math.sin(angle);
+
+    // Candle body (rectangle)
+    let candle = new Konva.Rect({
+      x: randX - 3,
+      y: randY - 20,
+      width: 6,
+      height: 20,
+      fill: "white",
+      stroke: "black",
+      strokeWidth: 2,
+    });
+
+    // Flame (ellipse above candle)
+    let flame = new Konva.Ellipse({
+      x: randX,
+      y: randY - 28,
+      radiusX: 4,
+      radiusY: 6,
+      fill: "orange",
+      stroke: "red",
+      strokeWidth: 4,
+    });
+
+ 
+
+    flame.on("click", () => {
+      candle.destroy();
+      flame.destroy();
+      layer.draw();
+      this.candlesBlown++;
+      console.log(this.candlesBlown);
+      this.blownCheck();
+    });
+    
+    layer.add(candle);
+    layer.add(flame);
+  
+}
+
+
+
+  topElip(color = "yellow") {
+    let topElipse = new Konva.Ellipse({
+      x: this.x,
+      y: this.y,
+      radiusX: this.radX,
+      radiusY: this.radY,
+      fill: color,
+      stroke: "black",
+      strokeWidth: 4,
+    });
+
+    layer.add(topElipse);
+  }
+
+  midElip(color = "yellow") {
+    let midElipse = new Konva.Ellipse({
+      x: this.x,
+      y: this.y + this.radX / 2,
+      radiusX: this.radX,
+      radiusY: this.radY,
+      fill: color,
+      stroke: "black",
+      strokeWidth: 4,
+    });
+
+    layer.add(midElipse);
+  }
+
+  botElip(color = "yellow") {
+    let botElipse = new Konva.Ellipse({
+      x: this.x,
+      y: this.y + this.radX,
+      radiusX: this.radX,
+      radiusY: this.radY,
+      fill: color,
+      stroke: "black",
+      strokeWidth: 4,
+    });
+
+    layer.add(botElipse);
+  }
+
+  leftLine() {
+    let lLine = new Konva.Line({
+      points: [this.x - this.radX, this.y, this.x - this.radX, this.y + this.radX],
+      stroke: "black",
+      strokeWidth: 4,
+      lineCap: "round",
+      lineJoin: "round",
+    });
+
+    layer.add(lLine);
+  }
+
+  rightLine() {
+    let rLine = new Konva.Line({
+      points: [this.x + this.radX, this.y, this.x + this.radX, this.y + this.radX],
+      stroke: "black",
+      strokeWidth: 4,
+      lineCap: "round",
+      lineJoin: "round",
+    });
+
+    layer.add(rLine);
+  }
+
+  rectangle(color = "yellow") {
+    let rect = new Konva.Rect({
+      x: this.x - this.radX,
+      y: this.y,
+      height: this.radX,
+      width: this.radX * 2,
+      fill: color,
+      strokeWidth: 4,
+    });
+
+    layer.add(rect);
+  }
+
+  topRectangle(color = "yellow") {
+    let topRect = new Konva.Rect({
+      x: this.x - this.radX,
+      y: this.y - 2,
+      height: this.radX / 2,
+      width: this.radX * 2,
+      fill: color,
+      strokeWidth: 4,
+    });
+    layer.add(topRect);
+  }
+
+  cakeGen() {
+    this.botElip("brown");
+    this.rectangle("brown");
+    this.midElip("white");
+    this.topRectangle("white");
+    this.topElip("white");
+    this.leftLine();
+    this.rightLine();
+    
+    for(let i = 0; i < 18; i++){
+      this.candles(i);
+    }
+    
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+ 
+
+
+  function main(){
+    let cake1 = new Cake(stage.width()/2, stage.height()/2, 2);
+    cake1.cakeGen();
+    
+  };
+
+  main();
+
+
+});
